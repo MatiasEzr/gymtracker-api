9ecff04ef3ed38ec64d0d906d041e789a701e3f4
Matías Lizarraga <Lizarraga_matias@hotmail.com.ar>
2026-04-15 00:28:26 -0300

    test: update application tests and test resources

--- src/test/java/com/matias/gymtracker/GymtrackerApplicationTests.java
+++ src/test/java/com/matias/gymtracker/GymtrackerApplicationTests.java
@@ -2,8 +2,10 @@ package com.matias.gymtracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GymtrackerApplicationTests {
 
 	@Test
